5bea18da8a778ffba6ee9ea4f7b62b3a0689e559
ming197 <ming197@qq.com>
2022-02-06 10:24:44 +0800

    two pointer

--- c++/.leetcode/80.remove-duplicates-from-sorted-array-ii.cpp
+++ c++/.leetcode/80.remove-duplicates-from-sorted-array-ii.cpp
@@ -10,7 +10,25 @@ using namespace std;
 class Solution {
 public:
     int removeDuplicates(vector<int>& nums) {
-        
+        int n = nums.size();
+        int i = 0, j = 0;
+        while (j < n)
+        {
+            /* code */
+            nums[i] = nums[j];
+            int x = nums[j];
+            if(j + 1 < n && nums[j + 1] == x){
+                nums[++i] = nums[++j];
+            }
+            while (j + 1 < n && nums[j+1] == x)
+            {
+                /* code */
+                j++;
+            }
+            i++;
+            j++;
+        }
+        return i;
     }
 };
 // @lc code=end
