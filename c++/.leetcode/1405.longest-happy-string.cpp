bd3fa58c0aea78fc33093c784ef62f12cc03f97f
ming197 <ming197@qq.com>
2022-02-07 12:59:26 +0800

    greedy

---
+++ c++/.leetcode/1405.longest-happy-string.cpp
@@ -0,0 +1,41 @@
+/*
+ * @lc app=leetcode id=1405 lang=cpp
+ *
+ * [1405] Longest Happy String
+ */
+
+// @lc code=start
+#include <bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    string longestDiverseString(int a, int b, int c) {
+        string res;
+        vector<pair<int, char>> arr = {{a, 'a'}, {b, 'b'}, {c, 'c'}};
+        while (true) {
+            sort(arr.begin(), arr.end(), [](const pair<int, char> & p1, const pair<int, char> & p2) {
+                return p1.first > p2.first;
+            });
+            bool hasNext = false;
+            for (auto & [freq, ch] : arr) {
+                int m = res.size();
+                if (freq <= 0) {
+                    break;
+                }
+                if (m >= 2 && res[m - 2] == ch && res[m - 1] == ch) {
+                    continue;
+                }
+                hasNext = true;
+                res.push_back(ch);
+                freq--;
+                break;
+            }
+            if (!hasNext) {
+                break;
+            }
+        }
+        return res;
+    }
+};
+// @lc code=end
+
