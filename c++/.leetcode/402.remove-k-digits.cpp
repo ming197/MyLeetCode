2c021c15c733a0fe736bbd493c06389d4e764bb2
ming197 <ming197@qq.com>
2022-02-18 15:11:44 +0800

    greedy, monotonic stk

---
+++ c++/.leetcode/402.remove-k-digits.cpp
@@ -0,0 +1,40 @@
+/*
+ * @lc app=leetcode id=402 lang=cpp
+ *
+ * [402] Remove K Digits
+ */
+
+// @lc code=start
+#include <bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    string removeKdigits(string num, int k) {
+        vector<char> stk;
+        for (auto& digit: num) {
+            while (stk.size() > 0 && stk.back() > digit && k) {
+                stk.pop_back();
+                k -= 1;
+            }
+            stk.push_back(digit);
+        }
+
+        for (; k > 0; --k) {
+            stk.pop_back();
+        }
+
+        string ans = "";
+        bool isLeadingZero = true;
+        for (auto& digit: stk) {
+            if (isLeadingZero && digit == '0') {
+                continue;
+            }
+            isLeadingZero = false;
+            ans += digit;
+        }
+        return ans == "" ? "0" : ans;
+    }
+};
+
+// @lc code=end
+
