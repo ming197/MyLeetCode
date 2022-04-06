7e84d6b1aa8bb3f7514bb1e137dd50f8a5c4dd4a
ming197 <ming197@qq.com>
2022-04-06 20:43:26 +0800

    hash map

---
+++ c++/.leetcode/923.3-sum-with-multiplicity.cpp
@@ -0,0 +1,31 @@
+/*
+ * @lc app=leetcode id=923 lang=cpp
+ *
+ * [923] 3Sum With Multiplicity
+ */
+
+// @lc code=start
+#include <bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    int threeSumMulti(vector<int>& A, int target) {
+        unordered_map<int, long> c;
+        for (int a : A) c[a]++;
+        long res = 0;
+        for (auto it : c)
+            for (auto it2 : c) {
+                int i = it.first, j = it2.first, k = target - i - j;
+                if (!c.count(k)) continue;
+                if (i == j && j == k)
+                    res += c[i] * (c[i] - 1) * (c[i] - 2) / 6;
+                else if (i == j && j != k)
+                    res += c[i] * (c[i] - 1) / 2 * c[k];
+                else if (i < j && j < k)
+                    res += c[i] * c[j] * c[k];
+            }
+        return res % int(1e9 + 7);
+    }
+};
+// @lc code=end
+
