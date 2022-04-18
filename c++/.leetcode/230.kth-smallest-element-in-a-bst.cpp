f16a959e95b3513e67a9453610111a05276c8707
ming197 <ming197@qq.com>
2022-04-18 09:53:13 +0800

    traverse in-order

--- c++/.leetcode/230.kth-smallest-element-in-a-bst.cpp
+++ c++/.leetcode/230.kth-smallest-element-in-a-bst.cpp
@@ -5,17 +5,17 @@
  */
 
 // @lc code=start
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+#include <bits/stdc++.h>
+using namespace std;
 class Solution {
 public:
     
