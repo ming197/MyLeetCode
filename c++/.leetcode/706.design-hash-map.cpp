622a6c83c8dcf4ecbd9104303ce631eff719d356
ming197 <ming197@qq.com>
2022-04-22 10:19:16 +0800

    design hashmap, vecot and linkedlist, stl

--- c++/.leetcode/706.design-hash-map.cpp
+++ c++/.leetcode/706.design-hash-map.cpp
@@ -5,22 +5,51 @@
  */
 
 // @lc code=start
+#include <bits/stdc++.h>
+using namespace std;
 class MyHashMap {
+private:
+    vector<list<pair<int, int>>> cache;
+    static const int base = 1 << 13;
+    static const int mask =  base - 1;
+    static int hash(int key) {
+        key ^= (key >> 16);
+        return key & mask;
+    }
 public:
-    MyHashMap() {
+    MyHashMap(): cache(base) {
         
     }
     
     void put(int key, int value) {
-        
+        int h = hash(key);
+        for(auto it = cache[h].begin(); it != cache[h].end(); it++){
+            if(it->first == key){
+                it->second = value;
+                return;
+            }
+        }
+        cache[h].push_back(make_pair(key, value));
     }
     
     int get(int key) {
-        
+        int h = hash(key);
+        for(auto it = cache[h].begin(); it != cache[h].end(); it++){
+            if(it->first == key){
+                return it->second;
+            }
+        }
+        return -1;
     }
     
     void remove(int key) {
-        
+        int h = hash(key);
+        for(auto it = cache[h].begin(); it != cache[h].end(); it++){
+            if(it->first == key){
+                cache[h].erase(it);
+                return;
+            }
+        }
     }
 };
 
