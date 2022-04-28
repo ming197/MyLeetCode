c8e2975bf9dc4c6b686d5dbf3c63d60e85d4b979
ming197 <ming197@qq.com>
2022-04-28 09:33:20 +0800

    vector

---
+++ c++/.leetcode/905.sort-array-by-parity.cpp
@@ -0,0 +1,33 @@
+/*
+ * @lc app=leetcode id=905 lang=cpp
+ *
+ * [905] Sort Array By Parity
+ */
+
+// @lc code=start
+#include <bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    vector<int> sortArrayByParity(vector<int>& nums) {
+        // int j = 0;
+        // for(int& x : nums){
+        //     if((x&1) == 1) j++;
+        // }
+        int i = 0;
+        for(int k = 0; k < nums.size(); k++){
+            if((nums[k] & 1) == 0){
+                swap(nums, i++, k);
+            }
+        }
+        return nums;
+    }
+
+    void swap(vector<int>& nums, int i, int j){
+        int tmp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = tmp;
+    }
+};
+// @lc code=end
+
