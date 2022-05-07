bae006ada8ad3abceab431e74329348382e11201
ming197 <ming197@qq.com>
2022-05-07 10:26:30 +0800

    bfs

---
+++ c++/.leetcode/433.minimum-genetic-mutation.cpp
@@ -0,0 +1,56 @@
+/*
+ * @lc app=leetcode id=433 lang=cpp
+ *
+ * [433] Minimum Genetic Mutation
+ */
+
+// @lc code=start
+#include <bits/stdc++.h>
+using namespace std;
+class Solution {
+public:    
+    int minMutation(string start, string end, vector<string>& bank) {
+        unordered_set<string> cnt;
+        unordered_set<string> visited;
+        char keys[4] = {'A', 'C', 'G', 'T'};        
+        for (auto & w : bank) {
+            cnt.emplace(w);
+        }
+        if (start == end) {
+            return 0;
+        }
+        if (!cnt.count(end)) {
+            return -1;
+        }
+        queue<string> qu;
+        qu.emplace(start);
+        visited.emplace(start);
+        int step = 1;
+        while (!qu.empty()) {
+            int sz = qu.size();
+            for (int i = 0; i < sz; i++) {
+                string curr = qu.front();
+                qu.pop();
+                for (int j = 0; j < 8; j++) {
+                    for (int k = 0; k < 4; k++) {
+                        if (keys[k] != curr[j]) {
+                            string next = curr;
+                            next[j] = keys[k];
+                            if (!visited.count(next) && cnt.count(next)) {
+                                if (next == end) {
+                                    return step;
+                                }
+                                qu.emplace(next);
+                                visited.emplace(next);
+                            }
+                        }
+                    }
+                }
+            }
+            step++;
+        }
+        return -1;
+    }
+};
+// @lc code=end
+
