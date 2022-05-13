b3283d1f2d56f9a09d4b89214539e00f1280c45c
ming197 <ming197@qq.com>
2022-05-13 23:15:55 +0800

    bfs

---
+++ c++/.leetcode/117.populating-next-right-pointers-in-each-node-ii.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Node {
+public:
+    int val;
+    Node* left;
+    Node* right;
+    Node* next;
+
+    Node() : val(0), left(NULL), right(NULL), next(NULL) {}
+
+    Node(int _val) : val(_val), left(NULL), right(NULL), next(NULL) {}
+
+    Node(int _val, Node* _left, Node* _right, Node* _next)
+        : val(_val), left(_left), right(_right), next(_next) {}
+};
+
+
+class Solution {
+public:
+    Node* connect(Node* root) {
+        if(root == nullptr) return root;
+        queue<Node*> q;
+        q.push(root);
+        while (!q.empty())
+        {
+            /* code */
+            int n = q.size();
+            for(int i = 0; i < n; i++){
+                Node* node = q.front();
+                q.pop();
+                if(node->left) q.push(node->left);
+                if(node->right) q.push(node->right);
+                if(i == n - 1) node->next = nullptr;
+                else{
+                    node->next = q.front();
+                }
+            }
+        }
+        return root;
+    }
+};
