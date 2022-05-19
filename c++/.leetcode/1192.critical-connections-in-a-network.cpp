73e353ab93537271031c2cd1f8a12850ce15424f
ming197 <ming197@qq.com>
2022-05-19 13:29:54 +0800

    find Kth minimal num

---
+++ c++/.leetcode/1192.critical-connections-in-a-network.cpp
@@ -0,0 +1,17 @@
+/*
+ * @lc app=leetcode id=1192 lang=cpp
+ *
+ * [1192] Critical Connections in a Network
+ */
+
+// @lc code=start
+#include <bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    vector<vector<int>> criticalConnections(int n, vector<vector<int>>& connections) {
+        
+    }
+};
+// @lc code=end
+
---
+++ c++/.leetcode/462.minimum-moves-to-equal-array-elements-ii.cpp
@@ -0,0 +1,22 @@
+/*
+ * @lc app=leetcode id=462 lang=cpp
+ *
+ * [462] Minimum Moves to Equal Array Elements II
+ */
+
+// @lc code=start
+#include <bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    int minMoves2(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+        int n = nums.size(), ret = 0, x = nums[n / 2];
+        for (int i = 0; i < n; i++) {
+            ret += abs(nums[i] - x);
+        }
+        return ret;
+    }
+};
+// @lc code=end
+
